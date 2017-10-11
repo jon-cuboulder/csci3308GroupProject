0a28674185fc7ce476c58f3e085e7cf9f0e8bb07
Jon <jon.mcmillan@colorado.edu>
2017-10-10 22:42:17 -0600

    Working cross origin request with ajax.  So frontend talking to php despite being on different ports
    whoops extra
    
    clear tab

---
+++ api/app/Http/Middleware/Cors.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+  /**
+   * Allow cross origin requests ... FROM EVERYWHERE. Need CORS so we can
+   * host javascript client independent of php api server. Do not need to allow
+   * all origins but for now this works. It's bad security to allow it from everywhere.
+   *
+   * http://en.vedovelli.com.br/2015/web-development/Laravel-5-1-enable-CORS/
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @return mixed
+   */
+  public function handle($request, Closure $next)
+  {
+    return $next($request)
+      ->header('Access-Control-Allow-Origin', '*')
+      ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  }
+}

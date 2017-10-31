8a30854de74ab93b532374559f8fec60c994a8a7
ehightow0206 <ehightow0206@gmail.com>
2017-10-31 11:59:05 -0600

    addeduser_id to resources table

---
+++ api/database/migrations/2017_10_31_174858_add_user_to_resources_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserToResourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('resources', function (Blueprint $table) {
+            //
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('resources', function (Blueprint $table) {
+            //
+            $table->dropForeign('user_id');
+        });
+    }
+}

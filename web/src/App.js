8f31cf58e4d5635c41d18126316f4cb65f4224af
Jon McMillan <jon.mcmillan@colorado.edu>
2017-11-09 18:29:12 -0700

    add a route to create an account and some boilerplate

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
 
 import searchReducer from './reducers/search';
 import Home from './containers/Home';
+import Register from './components/Register';
+import Navbar from './components/Navbar';
 
 const store = createStore(combineReducers({
   search: searchReducer
@@ -13,9 +20,15 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <Home />
-        </div>
+        <Router>
+          <div>
+            <Navbar />
+            <div className="container-fluid">
+              <Route exact path="/" component={Home} />
+              <Route path="/register" component={Register} />
+            </div>
+          </div>
+        </Router>
       </Provider>
     );
   }

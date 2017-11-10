d6a094eed767a2662db2d4ae49a451ee0f203fb7
Jon McMillan <jon.mcmillan@colorado.edu>
2017-11-09 17:05:49 -0700

    going to consistently expect event to be passed to handlers in the container ... hopefully making the indirection a little more intuitive to people that try to do this

--- web/src/containers/Home.js
+++ web/src/containers/Home.js
@@ -2,19 +2,16 @@ import { connect } from 'react-redux';
 import Home from '../components/Home';
 import * as search from '../actions/search';
 
-const mapStateToProps = (state) => {
-  console.log(state);
-  return {
+const mapStateToProps = (state) => ({
   qry: state.search.qry
-  };
-};
+});
 
 const mapDispatchToProps = dispatch => ({
-  onSubmit: (qry) => (e) => {
-    e.preventDefault();
+  onSubmit: (qry) => (event) => {
+    event.preventDefault();
     dispatch(search.submit(qry));
   },
-  handleChange: (text) => dispatch(search.handleChange(text))
+  handleChange: (event) => dispatch(search.handleChange(event.target.value))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);

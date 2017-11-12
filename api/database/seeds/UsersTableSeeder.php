2a7a26df0f0d721dd7eb8b2d87bc6ddb4ade80be
Jon McMillan <jon.mcmillan@colorado.edu>
2017-11-12 00:51:05 -0700

    add a topics seeder.  use factory to generate both topic and user

--- api/database/seeds/UsersTableSeeder.php
+++ api/database/seeds/UsersTableSeeder.php
@@ -11,17 +11,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-    	for($i=0; $i<50;$i++){
-    		DB::table('users')->insert([
-            'name' => str_random(10),
-            'email' => str_random(10).'@gmail.com',
-            'password' => bcrypt('secret'),
-        ]);
-    	}
-         DB::table('users')->insert([
-            'name' => str_random(10),
-            'email' => str_random(10).'@gmail.com',
-            'password' => bcrypt('secret'),
-        ]);
+        factory(App\User::class, 50)->create();
     }
 }

f6146dc6210e2732ca970b17a58cd53078786cec
Jon McMillan <jon.mcmillan@colorado.edu>
2017-11-20 12:44:00 -0700

    update resource list when one is added

--- web/src/actions/topics.js
+++ web/src/actions/topics.js
@@ -1,4 +1,19 @@
+/*
+ * Action Types
+ */
+export const RESOURCE_ADD = 'RESOURCE_ADD';
 export const TOPIC_LOAD = 'TOPIC_LOAD';
+
+/*
+ * Action Creators
+ */
+export function addResource(topicId, payload) {
+  return {
+    type: RESOURCE_ADD,
+    topicId,
+    payload
+  };
+}
 
 export function load(payload) {
   return {

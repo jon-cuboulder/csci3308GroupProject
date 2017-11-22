124e6222b8565f4a8aac54fd5742967f57e7cd4b
Jon McMillan <jon.mcmillan@colorado.edu>
2017-11-21 21:28:26 -0700

    re-add db script so resetting db is easier

--- setup.sh
+++ setup.sh
@@ -55,34 +55,7 @@ sudo service apache2 restart
 (cd api && cp .env.example .env)
 (cd api && php artisan key:generate)
 
-source api/.env
-
-checkIfEmpty() {
-  # ${!1} is a variable variable
-  # Just checking that the variable is set in the environment
-  if [ -z "${!1}" ] 
-    then
-      echo "ERROR: \$$1 must be set in the env prior to running the script."
-      exit 1
-  fi
-}
-
-checkIfEmpty DB_DATABASE
-checkIfEmpty DB_HOST
-checkIfEmpty DB_USERNAME
-
-if [ -z "$DB_ADMIN_PASSWORD" ]
-then
-  read -s -p "Enter MySQL root password: " DB_ADMIN_PASSWORD
-fi
-
-db="\`$DB_DATABASE\`"
-user="\`$DB_USERNAME\`@\`$DB_HOST\`" 
-query0="DROP DATABASE IF EXISTS $db"
-query1="CREATE USER IF NOT EXISTS $user IDENTIFIED BY '$DB_PASSWORD'"
-query2="CREATE DATABASE IF NOT EXISTS $db" 
-query3="GRANT ALL ON $db.* TO $user"
-
-echo "$query0;$query1;$query2;$query3;" | mysql -u $DB_ADMIN_USERNAME -p$DB_ADMIN_PASSWORD
-
-(cd api && php artisan migrate --seed)
+#
+# Setup DB
+#
+(cd api && ./db-setup.sh)

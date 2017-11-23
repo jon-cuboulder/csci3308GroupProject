540437836d34de7f29b6394a6114e2b298acb9e1
Jon McMillan <jon.mcmillan@colorado.edu>
2017-11-22 21:13:43 -0700

    layout topic page to prep for adding comments

--- web/src/components/Topic.js
+++ web/src/components/Topic.js
@@ -5,16 +5,15 @@ import Resource from '../containers/Resource';
 export default function Topic({ id, name, resources, vote }) {
   let list = <div>No resources</div>;
   if (resources.length > 0) {
-    list = <ul>
+    list = <ul className="list-group list-group-flush">
       {resources.map(r => 
-        <li key={r.id}> <Resource topicId={id} resourceId={r.id} /> </li>)}
+        <li className="list-group-item" key={r.id}> <Resource topicId={id} resourceId={r.id} /> </li>)}
       </ul>;
   }
 
   return (
-    <div>
-      <h2>{name}</h2>
-      <h4>Resources</h4>
+    <div className="mt-3">
+      <h3>{name}</h3>
       {list}
       <ResourceForm topicID={id} />
     </div>

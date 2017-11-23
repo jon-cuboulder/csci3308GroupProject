e039984fbc15981ef9c3ce3d053c5d92ec8620c7
Jon McMillan <jon.mcmillan@colorado.edu>
2017-11-23 10:21:25 -0700

    force sign in before showing comment form

--- web/src/components/CommentForm.js
+++ web/src/components/CommentForm.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Comment on a resource
-export default function CommentForm({ cancel, change, comment, resourceId, submit, isLoading }) {
+export default function CommentForm({ cancel, change, comment, resourceId, submit, isLoading, isAuthed }) {
+  if (!isAuthed) {
+    return (<div className="mt-2">
+      Please
+      <Link className="ml-3 mr-3 btn btn-outline-dark" to="/signin">
+        Sign In
+      </Link>
+      to add a comment.
+    </div>);
+  }
+
   return (<form onSubmit={submit(resourceId, comment)}>
     <textarea className="form-control" placeholder="Comment..." autoFocus 
     value={comment} onChange={change('comment')}/>

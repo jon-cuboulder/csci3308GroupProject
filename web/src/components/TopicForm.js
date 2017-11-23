765431846a6de3040e1cc58a02972d6c7c6799ca
Jon McMillan <jon.mcmillan@colorado.edu>
2017-11-22 22:11:52 -0700

    some topic form standarization

--- web/src/components/TopicForm.js
+++ web/src/components/TopicForm.js
@@ -5,15 +5,23 @@ export default function TopicForm({handleChange, handleSubmit, name, history, is
     <form onSubmit={handleSubmit(name, history)}>
       <h2>Create Topic</h2>
       <hr />
-      <div className="form-group">
-        <label htmlFor="name">Name</label>
-        <input type="text" className="form-control" id="name"
-          value={name}
-          onChange={handleChange('name')} placeholder="New Topic Name" />
+      <div className="form-group row align-items-center">
+        <div className="col-3 text-right">
+          <label htmlFor="name">Name</label>
+        </div>
+        <div className="col-6">
+          <input type="text" className="form-control" id="name" value={name}
+            onChange={handleChange('name')} placeholder="New Topic Name" />
+        </div>
       </div>
-      <button type="submit" className="btn btn-primary" disabled={isLoading}>
-        Submit
-      </button>
+      <div className="row">
+        <div className="offset-3 col-6">
+          <button type="submit" className="btn btn-success" disabled={isLoading}>
+            <span className="fa fa-plus mr-2" />
+            Add Topic
+          </button>
+        </div>
+      </div>
     </form>
-  );
+);
 }

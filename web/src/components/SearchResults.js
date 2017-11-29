ee66fbe81fa2744fae06fc5a093f51d4464d9502
Jon McMillan <jon.mcmillan@colorado.edu>
2017-11-28 17:23:28 -0700

    clear search when someone clicks on a topic

--- web/src/components/SearchResults.js
+++ web/src/components/SearchResults.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-export default function SearchResults({ results }) {
+export default function SearchResults({ results, clearSearch }) {
   let search = <div />;
   if(results.length) {
     search = (<div>
       <h4>Search Results</h4>
       <ul className="list-group list-group-flush">
         {results.map(result => <li className="list-group-item" key={result.id}>
-          <Link to={`/topics/${result.id}`}>{result.name}</Link>
+          <Link onClick={clearSearch} to={`/topics/${result.id}`}>{result.name}</Link>
         </li>)}
       </ul>
     </div>);

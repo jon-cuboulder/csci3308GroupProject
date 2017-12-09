df4e03ec7d72e6da8ad3f93616ba99eb6fa553b6
Jon McMillan <jon.mcmillan@colorado.edu>
2017-12-09 09:47:33 -0700

    disable button while submitting resource edit updates

--- web/src/containers/ResourceEditForm.js
+++ web/src/containers/ResourceEditForm.js
@@ -4,30 +4,37 @@ import * as forms from '../actions/forms';
 import * as api from '../api'
 import * as topics from '../actions/topics'
 
+const formName = resourceId => `resource-edit-${resourceId}`
 
 const mapStateToProps = (state, ownProps) => {
-	const form =  state.form["resource-edit-" + ownProps.resourceId];
-	let value = ownProps.name;
-	if(form) {
-		value = form.name;
-	}
-	return {
-		value
-	};
+  const form =  state.form[formName(ownProps.resourceId)];
+  let value = ownProps.name;
+  let isLoading = false;
+  if(form) {
+    value = form.name;
+    isLoading = form._loading;
+  }
+
+  return {
+    value,
+    isLoading
+  };
 };
 
 const mapDispatchToProps = dispatch => ({
-	handleChange: resourceId => forms.change(`resource-edit-${resourceId}`, dispatch),
-	handleSubmit: (topicId, resourceId, value) => e => {
-		e.preventDefault();
+  handleChange: resourceId => forms.change(`resource-edit-${resourceId}`, dispatch),
+  handleSubmit: (topicId, resourceId, value) => e => {
+    e.preventDefault();
+    dispatch(forms.loading(formName(resourceId), true));
 
-		api.editResourceName(resourceId, value)
-			.then( json => {
-				dispatch(topics.editResource(topicId, resourceId, {name: value}));
-				dispatch(topics.toggleEdit(topicId, resourceId));
-			})
-			.catch(err => alert(err.error));
-	}
+    api.editResourceName(resourceId, value)
+      .then( json => {
+        dispatch(topics.editResource(topicId, resourceId, {name: value}));
+        dispatch(topics.toggleEdit(topicId, resourceId));
+      })
+      .catch(err => alert(err.error))
+      .then( l => dispatch(forms.loading(formName(resourceId), false)));
+  }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ResourceEditForm);
